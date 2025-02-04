884fe83d72bb0dfd135077e8bd9b494cab2aabf1
nyuruN <zhangwai.herbst@gmail.com>
2025-02-04 23:31:29 +0000

    Disable Texture2DNode pins when none is selected

--- include/nodes.h
+++ include/nodes.h
@@ -476,31 +476,36 @@ public:
     this->texture = texture;
   }
   int get_output_pin() { return output_pin; }
+  void render_combobox(RenderGraph &graph) {
+    ImGui::SetNextItemWidth(node_width);
+    if (ImGui::BeginCombo("##hidelabel",
+                          bool(texture) ? texture->get_name().c_str() : "")) {
+      for (auto const &pair : *graph.textures) {
+        bool is_selected =
+            (texture) && (texture->get_name() == pair.second->get_name());
+        if (ImGui::Selectable(pair.first.c_str(), is_selected))
+          set_texture(pair.second);
+        // Set the initial focus when opening the combo (scrolling + keyboard
+        // navigation focus)
+        if (is_selected)
+          ImGui::SetItemDefaultFocus();
+      }
+      ImGui::EndCombo();
+    }
+  }
   void render(RenderGraph &graph) override {
     ImNodes::BeginNode(id);
 
     ImNodes::BeginNodeTitleBar();
     ImGui::Text("Texture Node");
     ImNodes::EndNodeTitleBar();
-    {
-      ImNodes::BeginOutputAttribute(output_pin);
-      ImGui::SetNextItemWidth(node_width);
 
-      if (ImGui::BeginCombo("##hidelabel",
-                            bool(texture) ? texture->get_name().c_str() : "")) {
-        for (auto const &pair : *graph.textures) {
-          bool is_selected =
-              (texture) && (texture->get_name() == pair.second->get_name());
-          if (ImGui::Selectable(pair.first.c_str(), is_selected))
-            set_texture(pair.second);
-          // Set the initial focus when opening the combo (scrolling + keyboard
-          // navigation focus)
-          if (is_selected)
-            ImGui::SetItemDefaultFocus();
-        }
-        ImGui::EndCombo();
-      }
+    if (texture) {
+      ImNodes::BeginOutputAttribute(output_pin);
+      render_combobox(graph);
       ImNodes::EndOutputAttribute();
+    } else {
+      render_combobox(graph);
     }
 
     ImGui::Dummy(ImVec2(node_width, 20));
